b3fbd39f1ab8b6a7600a38f8bda169b1ec82d8f7
wiyco <72733890+wiyco@users.noreply.github.com>
2024-10-29 08:50:41 +0900

    feat(core): type definitions for react components

---
+++ src/core/ui/react.types.d.ts
@@ -0,0 +1,78 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+/**
+ * Acknowledgments:
+ * This code is derived from and inspired by Chakra UI.
+ * Special thanks to the Chakra UI team for their outstanding work and open-source contributions.
+ * Their implementation patterns and design principles have significantly influenced this code.
+ *
+ * Original source:
+ * @see {@link https://github.com/chakra-ui/chakra-ui/blob/next/packages/system/src/system.types.tsx | @chakra-ui/system/src/system.types.tsx}
+ *
+ * Chakra UI - Copyright (c) 2019 Segun Adebayo
+ * @license MIT
+ */
+
+/**
+ * An empty object type
+ */
+type EmptyObject = Record<string, never>;
+
+type As<Props = any> = React.ElementType<Props>;
+
+/**
+ * Extract the props of a React element or component
+ */
+type PropsOf<T extends As> = React.ComponentPropsWithoutRef<T> & {
+  as?: As;
+};
+
+type OmitCommonProps<
+  Target,
+  OmitAdditionalProps extends keyof any = never,
+> = Omit<Target, "transition" | "as" | "color" | OmitAdditionalProps>;
+
+type RightJoinProps<
+  SourceProps extends object = EmptyObject,
+  OverrideProps extends object = EmptyObject,
+> = OmitCommonProps<SourceProps, keyof OverrideProps> & OverrideProps;
+
+type MergeWithAs<
+  ComponentProps extends object,
+  AsProps extends object,
+  AdditionalProps extends object = EmptyObject,
+  AsComponent extends As = As,
+> = RightJoinProps<ComponentProps, AdditionalProps> &
+  RightJoinProps<AsProps, AdditionalProps> & {
+    as?: AsComponent;
+  };
+
+type ComponentWithAs<
+  Component extends As,
+  Props extends object = EmptyObject,
+> = {
+  <AsComponent extends As = Component>(
+    props: MergeWithAs<
+      React.ComponentProps<Component>,
+      React.ComponentProps<AsComponent>,
+      Props,
+      AsComponent
+    >
+  ): JSX.Element;
+
+  displayName?: string;
+  propTypes?: React.WeakValidationMap<any>;
+  contextTypes?: React.ValidationMap<any>;
+  defaultProps?: Partial<any>;
+  id?: string;
+};
+
+export type {
+  As,
+  ComponentWithAs,
+  EmptyObject,
+  MergeWithAs,
+  OmitCommonProps,
+  PropsOf,
+  RightJoinProps,
+};
