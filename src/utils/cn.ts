2c836fc91cb9dea4f05b4ac54af16b05bbac88df
wiyco <72733890+wiyco@users.noreply.github.com>
2024-10-29 23:10:35 +0900

    refactor(utils): use inline export /cn

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -1,10 +1,8 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-function cn(
+export function cn(
   ...classNames: Parameters<typeof clsx> & Parameters<typeof twMerge>
 ): string {
   return twMerge(clsx(classNames));
 }
-
-export { cn };
