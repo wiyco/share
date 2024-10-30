feace007c5479a51c20e3e65f5895414801ee3b7
wiyco <72733890+wiyco@users.noreply.github.com>
2024-10-30 10:17:24 +0900

    refactor(card-footer): remove unnecessary types

--- src/components/card/card-footer.tsx
+++ src/components/card/card-footer.tsx
@@ -3,9 +3,7 @@ import { tv } from "tailwind-variants";
 import { forwardRef, type HTMLComponentProps } from "@/core";
 import { cn } from "@/utils";
 
-export interface CardFooterProps extends HTMLComponentProps<"span"> {
-  as?: React.ElementType;
-}
+export interface CardFooterProps extends HTMLComponentProps<"span"> {}
 
 const variants = tv({
   base: "absolute inset-x-0 left-3 right-3 bottom-3 h-[clamp(fit-content,25%,5rem)] grid place-content-center px-4 py-3.5 rounded-xl overflow-clip bg-white/60 backdrop-blur-lg text-center text-neutral-900 text-sm",
